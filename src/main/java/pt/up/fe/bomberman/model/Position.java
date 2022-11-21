c4c6f9c2d793bef01141cbf5a4a7e97c0370fed8
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-21 12:05:09 +0000

    Finished class Position with equals and hashCode

--- src/main/java/pt/up/fe/bomberman/model/Position.java
+++ src/main/java/pt/up/fe/bomberman/model/Position.java
@@ -1,5 +1,7 @@
 package pt.up.fe.bomberman.model;
 
+import java.util.Objects;
+
 public class Position {
     private final int x;
     private final int y;
@@ -41,5 +43,18 @@ public class Position {
             case 2 -> getDown();
             default -> getLeft();
         };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
