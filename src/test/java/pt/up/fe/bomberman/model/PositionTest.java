62d5f64f8805432dda257effbf715aa063cc7ea1
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-21 11:59:32 +0000

    Created test getRight for class Position

--- src/test/java/pt/up/fe/bomberman/model/PositionTest.java
+++ src/test/java/pt/up/fe/bomberman/model/PositionTest.java
@@ -12,4 +12,11 @@ class PositionTest {
         assertEquals(x, position.getUp().getX());
         assertEquals(y - 1, position.getUp().getY());
     }
+
+    @Property
+    void getRight(@ForAll int x, @ForAll int y) {
+        Position position = new Position(x, y);
+        assertEquals(x + 1, position.getUp().getX());
+        assertEquals(y, position.getUp().getY());
+    }
 }
