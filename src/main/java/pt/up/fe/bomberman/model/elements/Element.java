c1e7ba776ec1b40fe12a6c5b38a9c86d6930e9ec
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-21 12:10:07 +0000

    Created class element

---
+++ src/main/java/pt/up/fe/bomberman/model/elements/Element.java
@@ -0,0 +1,19 @@
+package pt.up.fe.bomberman.model.elements;
+
+import pt.up.fe.bomberman.model.Position;
+
+public class Element {
+    private Position position;
+
+    public Element(int x, int y) {
+        position = new Position(x, y);
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
