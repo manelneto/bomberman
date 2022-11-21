275585da9d4e75a3749828fd6344f716ecd8636a
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-21 18:41:39 +0000

    Added GUI interface

---
+++ src/main/java/pt/up/fe/bomberman/gui/GUI.java
@@ -0,0 +1,23 @@
+package pt.up.fe.bomberman.gui;
+
+import pt.up.fe.bomberman.model.Position;
+
+public interface GUI {
+    enum ACTION {UP, RIGHT, DOWN, LEFT, ENTER}
+
+    ACTION getNextAction();
+
+    void drawBomberman(Position position);
+
+    void drawObstacle(Position position);
+
+    void drawWall(Position position);
+
+    void drawEnemy(Position position);
+
+    void clear();
+
+    void refresh();
+
+    void close();
+}
