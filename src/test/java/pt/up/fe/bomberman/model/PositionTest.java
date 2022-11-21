ee3e668f279beca37c41bb521f7fc6f08a311459
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-21 11:57:24 +0000

    Created test getUp for class Position

---
+++ src/test/java/pt/up/fe/bomberman/model/PositionTest.java
@@ -0,0 +1,15 @@
+package pt.up.fe.bomberman.model;
+
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PositionTest {
+    @Property
+    void getUp(@ForAll int x, @ForAll int y) {
+        Position position = new Position(x, y);
+        assertEquals(x, position.getUp().getX());
+        assertEquals(y - 1, position.getUp().getY());
+    }
+}
