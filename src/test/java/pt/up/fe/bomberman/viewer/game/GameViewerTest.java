21286d5dbb46afba10496bb79824eced8fae9ae9
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-22 21:25:01 +0000

    Added drawObstacles test

--- src/test/java/pt/up/fe/bomberman/viewer/game/GameViewerTest.java
+++ src/test/java/pt/up/fe/bomberman/viewer/game/GameViewerTest.java
@@ -27,7 +27,7 @@ public class GameViewerTest {
 
         arena.setBomberman(new Bomberman(1, 1));
         arena.setEnemies(Arrays.asList(new Enemy(5, 5), new Enemy(7, 7)));
-        arena.setObstacles(Arrays.asList(new Obstacle(10, 10)));
+        arena.setObstacles(Arrays.asList(new Obstacle(10, 10), new Obstacle(12, 12), new Obstacle(14, 14)));
         arena.setWalls(Arrays.asList(new Wall(15, 15)));
     }
 
@@ -46,5 +46,15 @@ public class GameViewerTest {
         Mockito.verify(gui, Mockito.times(1)).drawEnemy(new Position(5, 5));
         Mockito.verify(gui, Mockito.times(1)).drawEnemy(new Position(7, 7));
         Mockito.verify(gui, Mockito.times(2)).drawEnemy(Mockito.any(Position.class));
+    }
+
+    @Test
+    void drawObstacles() throws IOException {
+        viewer.draw(gui);
+
+        Mockito.verify(gui, Mockito.times(1)).drawObstacle(new Position(10, 10));
+        Mockito.verify(gui, Mockito.times(1)).drawObstacle(new Position(12, 12));
+        Mockito.verify(gui, Mockito.times(1)).drawObstacle(new Position(14, 14));
+        Mockito.verify(gui, Mockito.times(3)).drawEnemy(Mockito.any(Position.class));
     }
 }
