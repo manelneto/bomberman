54717f5ec2e3fe506f974828a567c5b9231824f2
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-22 17:51:36 +0000

    Added drawBomberman() test for class LanternaGUI

---
+++ src/test/java/pt/up/fe/bomberman/gui/LaternaGUITest.java
@@ -0,0 +1,33 @@
+package pt.up.fe.bomberman.gui;
+
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pt.up.fe.bomberman.model.Position;
+
+class LaternaGUITest {
+    private Screen screen;
+    private LanternaGUI gui;
+    private TextGraphics textGraphics;
+
+    @BeforeEach
+    void setUp() {
+        screen = Mockito.mock(Screen.class);
+        textGraphics = Mockito.mock(TextGraphics.class);
+
+        Mockito.when(screen.newTextGraphics()).thenReturn(textGraphics);
+
+        gui = new LanternaGUI(screen);
+    }
+
+    @Test
+    void drawBomberman() {
+        gui.drawBomberman(new Position(10, 10));
+
+        Mockito.verify(textGraphics, Mockito.times(1)).setForegroundColor(new TextColor.RGB(255, 0, 0));
+        Mockito.verify(textGraphics, Mockito.times(1)).putString(10, 10, "B");
+    }
+}
