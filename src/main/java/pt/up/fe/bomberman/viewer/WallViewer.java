d8ab428c5c303e085aa323de30bf66f2d6f688a2
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-22 19:17:32 +0000

    Added drawWall test for WallViewer

---
+++ src/main/java/pt/up/fe/bomberman/viewer/WallViewer.java
@@ -0,0 +1,11 @@
+package pt.up.fe.bomberman.viewer;
+
+import pt.up.fe.bomberman.gui.GUI;
+import pt.up.fe.bomberman.model.elements.Wall;
+
+public class WallViewer implements ElementViewer<Wall> {
+    @Override
+    public void draw(Wall wall, GUI gui) {
+
+    }
+}
