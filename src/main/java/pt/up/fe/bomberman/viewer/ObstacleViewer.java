8890d18298c7de3510a547305132ad5eb4ffd3d4
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-22 19:13:08 +0000

    Added drawObstacle test for ObstacleViewer

---
+++ src/main/java/pt/up/fe/bomberman/viewer/ObstacleViewer.java
@@ -0,0 +1,11 @@
+package pt.up.fe.bomberman.viewer;
+
+import pt.up.fe.bomberman.gui.GUI;
+import pt.up.fe.bomberman.model.elements.Obstacle;
+
+public class ObstacleViewer implements ElementViewer<Obstacle> {
+    @Override
+    public void draw(Obstacle element, GUI gui) {
+
+    }
+}
---
+++ src/test/java/pt/up/fe/bomberman/viewer/ObstacleViewerTest.java
@@ -0,0 +1,27 @@
+package pt.up.fe.bomberman.viewer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pt.up.fe.bomberman.gui.GUI;
+import pt.up.fe.bomberman.model.elements.Enemy;
+import pt.up.fe.bomberman.model.elements.Obstacle;
+
+public class ObstacleViewerTest {
+    private Obstacle obstacle;
+    private ObstacleViewer viewer;
+    private GUI gui;
+
+    @BeforeEach
+    void setUp() {
+        obstacle = new Obstacle(5, 5);
+        viewer = new ObstacleViewer();
+        gui = Mockito.mock(GUI.class);
+    }
+
+    @Test
+    void drawObstacle() {
+        viewer.draw(obstacle, gui);
+        Mockito.verify(gui, Mockito.times(1)).drawObstacle(obstacle.getPosition());
+    }
+}
