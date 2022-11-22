229e0542f3ebfec3710945735b53e4ddf4f9d1f7
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-22 20:38:48 +0000

    Added drawBomberman function

--- src/main/java/pt/up/fe/bomberman/viewer/game/GameViewer.java
+++ src/main/java/pt/up/fe/bomberman/viewer/game/GameViewer.java
@@ -2,6 +2,7 @@ package pt.up.fe.bomberman.viewer.game;
 
 import pt.up.fe.bomberman.gui.GUI;
 import pt.up.fe.bomberman.model.arena.Arena;
+import pt.up.fe.bomberman.model.elements.Element;
 import pt.up.fe.bomberman.viewer.Viewer;
 
 public class GameViewer extends Viewer<Arena> {
@@ -9,7 +10,11 @@ public class GameViewer extends Viewer<Arena> {
         super(arena);
     }
     @Override
-    protected void drawElements(GUI gui) {
+    public void drawElements(GUI gui) {
+        drawElement(gui, getModel().getBomberman(), new BombermanViewer());
+    }
 
+    private <T extends Element> void drawElement(GUI gui, T element, ElementViewer<T> viewer) {
+        viewer.draw(element, gui);
     }
 }
