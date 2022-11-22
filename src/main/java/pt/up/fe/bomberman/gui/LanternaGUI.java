d3936db0c8d1586f12ca81c29ad7abf80c671e42
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-22 18:35:03 +0000

    Added Obstacle class (only constructor)

--- src/main/java/pt/up/fe/bomberman/gui/LanternaGUI.java
+++ src/main/java/pt/up/fe/bomberman/gui/LanternaGUI.java
@@ -62,7 +62,7 @@ public class LanternaGUI implements GUI {
 
     @Override
     public void drawEnemy(Position position) {
-        drawCharacter(position.getX(), position.getY(), 'E', "#FF0000"); //red
+        drawCharacter(position.getX(), position.getY(), 'E', "#FF0000"); //redO
     }
 
     private void drawCharacter(int x, int y, char c, String color) {
---
+++ src/main/java/pt/up/fe/bomberman/model/elements/Obstacle.java
@@ -0,0 +1,7 @@
+package pt.up.fe.bomberman.model.elements;
+
+public class Obstacle extends Element {
+    public Obstacle(int x, int y) {
+        super(x, y);
+    }
+}
