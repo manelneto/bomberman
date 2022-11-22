55dfd2857ca6ce6cd5d8d6ab41793c56d47d1653
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-11-22 20:27:37 +0000

    Created class ArenaViewer

---
+++ src/main/java/pt/up/fe/bomberman/viewer/game/ArenaViewer.java
@@ -0,0 +1,15 @@
+package pt.up.fe.bomberman.viewer.game;
+
+import pt.up.fe.bomberman.gui.GUI;
+import pt.up.fe.bomberman.model.arena.Arena;
+import pt.up.fe.bomberman.viewer.Viewer;
+
+public class ArenaViewer extends Viewer<Arena> {
+    public ArenaViewer(Arena arena) {
+        super(arena);
+    }
+    @Override
+    protected void drawElements(GUI gui) {
+
+    }
+}
