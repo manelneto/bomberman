61f7a0b27aa71c801024f3b4b3d8e84e0d1ffa22
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-11 16:11:53 +0000

    removed unused import

--- src/main/java/pt/up/fe/bomberman/model/game/elements/powerup/Bombs.java
+++ src/main/java/pt/up/fe/bomberman/model/game/elements/powerup/Bombs.java
@@ -1,12 +1,9 @@
 package pt.up.fe.bomberman.model.game.elements.powerup;
 
-import pt.up.fe.bomberman.model.game.elements.Bomberman;
 import pt.up.fe.bomberman.model.game.elements.Powerup;
 
 public class Bombs extends Powerup {
     public Bombs(int x, int y) {
         super(x, y, Effect.Bombs);
     }
-
 }
-
