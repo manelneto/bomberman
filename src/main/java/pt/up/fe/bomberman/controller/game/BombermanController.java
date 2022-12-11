cf79d12b3a192472da1ac81427ec4e79a26cc246
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-11 16:54:26 +0000

    cleaned up code

--- src/main/java/pt/up/fe/bomberman/controller/game/BombermanController.java
+++ src/main/java/pt/up/fe/bomberman/controller/game/BombermanController.java
@@ -4,48 +4,44 @@ package pt.up.fe.bomberman.controller.game;
 import pt.up.fe.bomberman.Game;
 import pt.up.fe.bomberman.gui.GUI;
 import pt.up.fe.bomberman.model.Position;
-
 import pt.up.fe.bomberman.model.game.arena.Arena;
-import pt.up.fe.bomberman.model.game.elements.Bomb;
 import pt.up.fe.bomberman.model.game.elements.Powerup;
 
 public class BombermanController extends GameController {
-    private long lastMovementTime;
+    // private long lastMovementTime;
     public BombermanController(Arena arena) {
         super(arena);
     }
 
     public void moveBombermanLeft() {
-
-        getModel().getBomberman().Face_left();
-        moveBomber(getModel().getBomberman().getPosition().getLeft());
+        getModel().getBomberman().faceLeft();
+        moveBomberman(getModel().getBomberman().getPosition().getLeft());
     }
 
     public void moveBombermanRight() {
-        getModel().getBomberman().Face_right();
-        moveBomber(getModel().getBomberman().getPosition().getRight());
+        getModel().getBomberman().faceRight();
+        moveBomberman(getModel().getBomberman().getPosition().getRight());
     }
 
     public void moveBombermanUp() {
-        getModel().getBomberman().Face_Up();
-        moveBomber(getModel().getBomberman().getPosition().getUp());
+        getModel().getBomberman().faceUp();
+        moveBomberman(getModel().getBomberman().getPosition().getUp());
     }
 
     public void moveBombermanDown() {
-
-        getModel().getBomberman().Face_down();
-        moveBomber(getModel().getBomberman().getPosition().getDown());
+        getModel().getBomberman().faceDown();
+        moveBomberman(getModel().getBomberman().getPosition().getDown());
     }
 
-    private void moveBomber(Position position) {
+    private void moveBomberman(Position position) {
         if (getModel().isEmpty(position)) {
             getModel().getBomberman().setPosition(position);
-            if (getModel().isEnemy(position)) getModel().getBomberman().TakesHit();
-            else if (getModel().isPowerup(position)) applyEffect(getModel().findPowerup(position));
+            if (getModel().isEnemy(position)) getModel().getBomberman().decreaseHp();
+            if (getModel().isPowerup(position)) applyEffect(getModel().findPowerup(position));
         }
     }
 
-    private void applyEffect(Powerup powerup){
+    private void applyEffect(Powerup powerup) {
         switch (powerup.getEffect()) {
             case Bombpass -> getModel().getBomberman().ableBombpass();
             case Bombs -> getModel().getBomberman().increaseUsableBombs();
@@ -59,18 +55,12 @@ public class BombermanController extends GameController {
 
     @Override
     public void step(Game game, GUI.ACTION action, long time) {
-        if (time - lastMovementTime > (250/getModel().getBomberman().getSpeed())) {
-
-
-
-            if (action == GUI.ACTION.UP) {moveBombermanUp();this.lastMovementTime = time;}
-            if (action == GUI.ACTION.RIGHT){ moveBombermanRight();this.lastMovementTime = time;}
-            if (action == GUI.ACTION.DOWN){ moveBombermanDown();this.lastMovementTime = time;}
-            if (action == GUI.ACTION.LEFT){ moveBombermanLeft();this.lastMovementTime = time;}
-            if (getModel().HasBomb()==false && action == GUI.ACTION.SPACE ){ getModel().CreateBomb(time);this.lastMovementTime = time;}
-        }
+        //if (time - lastMovementTime > (250/getModel().getBomberman().getSpeed())) {
+            if (action == GUI.ACTION.UP) moveBombermanUp(); //this.lastMovementTime = time;}
+            if (action == GUI.ACTION.RIGHT) moveBombermanRight(); //this.lastMovementTime = time;}
+            if (action == GUI.ACTION.DOWN) moveBombermanDown(); //this.lastMovementTime = time;}
+            if (action == GUI.ACTION.LEFT) moveBombermanLeft(); //this.lastMovementTime = time;}
+            //if (getModel().HasBomb()==false && action == GUI.ACTION.SPACE ){ getModel().CreateBomb(time);this.lastMovementTime = time;}
+        //}
     }
-
-
 }
-
