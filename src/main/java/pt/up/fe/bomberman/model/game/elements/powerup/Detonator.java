18cccbb8a4820c2838f558f054c6ff055546e854
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-11 16:12:05 +0000

    removed unused import

--- src/main/java/pt/up/fe/bomberman/model/game/elements/powerup/Detonator.java
+++ src/main/java/pt/up/fe/bomberman/model/game/elements/powerup/Detonator.java
@@ -1,12 +1,9 @@
 package pt.up.fe.bomberman.model.game.elements.powerup;
 
-import pt.up.fe.bomberman.model.game.elements.Bomberman;
 import pt.up.fe.bomberman.model.game.elements.Powerup;
 
 public class Detonator extends Powerup {
     public Detonator(int x, int y) {
         super(x, y, Effect.Detonator);
     }
-
 }
-
