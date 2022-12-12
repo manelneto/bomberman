0659c0609ecdc7b861f2c13696ba9dc1d39c07f8
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-12 12:18:19 +0000

    changed StateTest

--- src/test/java/pt/up/fe/bomberman/States/StateTest.java
+++ src/test/java/pt/up/fe/bomberman/States/StateTest.java
@@ -1,67 +1,24 @@
-package pt.up.fe.bomberman.States;
-
+package pt.up.fe.bomberman.states;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import pt.up.fe.bomberman.controller.Controller;
-import pt.up.fe.bomberman.controller.game.GameController;
-import pt.up.fe.bomberman.controller.menu.MenuController;
-
-
 import pt.up.fe.bomberman.model.game.arena.Arena;
 import pt.up.fe.bomberman.model.menu.Menu;
-import pt.up.fe.bomberman.states.GameState;
-import pt.up.fe.bomberman.states.MenuState;
-import pt.up.fe.bomberman.states.State;
-import pt.up.fe.bomberman.viewer.Viewer;
-
-import java.awt.*;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-
 
 public class StateTest {
+    private MenuState menuState;
+    private GameState gameState;
 
-    private GameState Gmodel;
-
-    private MenuState Mmodel;
-
-    private Controller controllerMenu;
-
-    private Controller controllerGame;
-
-    private Viewer viewer;
-
-    @BeforeEach
-    void setUp() throws IOException, URISyntaxException, FontFormatException {
-
-        controllerGame = Mockito.mock(GameController.class);
-
-        controllerMenu = Mockito.mock(MenuController.class);
-
-        Mmodel=Mockito.mock(MenuState.class);
-
-        viewer=Mockito.mock(Viewer.class);
-
-        Gmodel=Mockito.mock(GameState.class);
-
-
-    }
     @Test
     void getModelTest() {
+        Menu menu = Mockito.mock(Menu.class);
+        menuState = new MenuState(menu);
+        Arena arena = Mockito.mock(Arena.class);
+        gameState = new GameState(arena);
 
-        Menu menu=Mockito.mock(Menu.class);
-        Mmodel= new MenuState(menu);
-        Arena arena= Mockito.mock(Arena.class);
-        Gmodel=new GameState(arena);
-
-        Assertions.assertTrue(Mmodel.getModel()== menu);
-        Assertions.assertTrue(Gmodel.getModel()== arena);
-
+        Assertions.assertEquals(menuState.getModel(), menu);
+        Assertions.assertEquals(gameState.getModel(), arena);
     }
-
 }
