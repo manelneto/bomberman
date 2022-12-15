6442963c64f18f2bad2e5610db9b3620f3191290
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-15 12:02:17 +0000

    changed PowerupViewer switch to instanceof

--- src/main/java/pt/up/fe/bomberman/viewer/game/PowerupViewer.java
+++ src/main/java/pt/up/fe/bomberman/viewer/game/PowerupViewer.java
@@ -2,28 +2,25 @@ package pt.up.fe.bomberman.viewer.game;
 
 import pt.up.fe.bomberman.gui.GUI;
 import pt.up.fe.bomberman.model.game.elements.Powerup;
+import pt.up.fe.bomberman.model.game.elements.powerup.*;
 
 public class PowerupViewer implements ElementViewer<Powerup> {
     @Override
     public void draw(Powerup powerup, GUI gui) {
-        switch (powerup.getEffect()){
-
-            case Bombpass:{gui.drawElement(powerup.getPosition(),'r', "#F7EF8A");break;}
-
-            case Bombs:{gui.drawElement(powerup.getPosition(),'e', "#F7EF8A");break;}
-
-            case Detonator:{gui.drawElement(powerup.getPosition(),'g', "#F7EF8A");break;}
-
-            case Flamepass:{gui.drawElement(powerup.getPosition(),'q', "#F7EF8A");break;}
-
-            case Flames:{gui.drawElement(powerup.getPosition(),'d', "#F7EF8A");break;}
-
-            case Speed:{gui.drawElement(powerup.getPosition(),'f', "#F7EF8A");break;}
-
-            case Wallpass:{gui.drawElement(powerup.getPosition(),'p', "#F7EF8A");break;}
-
-
-        }
+        if (powerup instanceof Bombs)
+            gui.drawElement(powerup.getPosition(),'e', "#F7EF8A");
+        if (powerup instanceof Flames)
+            gui.drawElement(powerup.getPosition(),'d', "#F7EF8A");
+        if (powerup instanceof Speed)
+            gui.drawElement(powerup.getPosition(),'f', "#F7EF8A");
+        if (powerup instanceof Wallpass)
+            gui.drawElement(powerup.getPosition(),'p', "#F7EF8A");
+        if (powerup instanceof Detonator)
+            gui.drawElement(powerup.getPosition(),'g', "#F7EF8A");
+        if (powerup instanceof Bombpass)
+            gui.drawElement(powerup.getPosition(),'r', "#F7EF8A");
+        if (powerup instanceof Flamepass)
+            gui.drawElement(powerup.getPosition(),'q', "#F7EF8A");
     }
 }
 
