ffadd5abe2c62986e4ac68f86d94e9a68981ef47
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-15 12:26:39 +0000

    spaces and identation

--- src/main/java/pt/up/fe/bomberman/controller/menu/MenuController.java
+++ src/main/java/pt/up/fe/bomberman/controller/menu/MenuController.java
@@ -1,12 +1,9 @@
-
 package pt.up.fe.bomberman.controller.menu;
 
 import pt.up.fe.bomberman.Game;
 import pt.up.fe.bomberman.controller.Controller;
 import pt.up.fe.bomberman.gui.GUI;
-
 import pt.up.fe.bomberman.model.game.arena.LoaderArenaBuilder;
-
 import pt.up.fe.bomberman.model.menu.Menu;
 import pt.up.fe.bomberman.states.GameState;
 
@@ -27,16 +24,16 @@ public class MenuController extends Controller<Menu> {
                 getModel().nextEntry();
                 break;
             case LEFT:
-                if(getModel().isSelectedLevel()) getModel().previousLevel();
+                if (getModel().isSelectedLevel())
+                    getModel().previousLevel();
                 break;
             case RIGHT:
-                if(getModel().isSelectedLevel()) getModel().nextLevel();
+                if (getModel().isSelectedLevel())
+                    getModel().nextLevel();
                 break;
             case ENTER:
                 if (getModel().isSelectedExit()) game.setState(null);
                 if (getModel().isSelectedStart()) game.setState(new GameState(new LoaderArenaBuilder(getModel().getLevel()).createArena()));
-
         }
     }
 }
-
