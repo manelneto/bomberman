23da18ea76c0a88e5ce7a39986ce17efac1a1013
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-22 17:22:47 +0000

    ended bomb reviewing and refactoring: added newline

--- src/main/java/pt/up/fe/bomberman/model/game/elements/Bomb.java
+++ src/main/java/pt/up/fe/bomberman/model/game/elements/Bomb.java
@@ -3,6 +3,7 @@ package pt.up.fe.bomberman.model.game.elements;
 public class Bomb extends Element {
     private final long time;
     private final int flames;
+    
     public Bomb(int x, int y, long time, int flames) {
         super(x, y);
         this.time = time;
