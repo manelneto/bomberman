b23f93f0738df2dbd0c4a902fdd7c13637c7c54e
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-22 18:09:05 +0000

    ended enemyViewer reviewing and refactoring: consistency and unused import

--- src/main/java/pt/up/fe/bomberman/viewer/game/EnemyViewer.java
+++ src/main/java/pt/up/fe/bomberman/viewer/game/EnemyViewer.java
@@ -2,28 +2,17 @@ package pt.up.fe.bomberman.viewer.game;
 
 import pt.up.fe.bomberman.gui.GUI;
 import pt.up.fe.bomberman.model.game.elements.Enemy;
-import pt.up.fe.bomberman.model.game.elements.enemy.*;
-
 
 public class EnemyViewer implements ElementViewer<Enemy> {
     @Override
     public void draw(Enemy enemy, GUI gui) {
-        if (enemy instanceof Balloom)
-            gui.drawElement(enemy.getPosition(), ':', "#FA732C");
-        if (enemy instanceof Oneal)
-            gui.drawElement(enemy.getPosition(), 'n', "#0000FF");
-        if (enemy instanceof Doll)
-            gui.drawElement(enemy.getPosition(), 'o', "#FF0000");
-        if (enemy instanceof Kondria)
-            gui.drawElement(enemy.getPosition(), 'u', "#0000FF");
-        if (enemy instanceof Minvo)
-            gui.drawElement(enemy.getPosition(), 't', "#FA732C");
-        if (enemy instanceof Ovapi)
-            gui.drawElement(enemy.getPosition(), 'v', "#FF0000");
-        if (enemy instanceof Pass)
-            gui.drawElement(enemy.getPosition(), 'w', "#FA732C");
-        if (enemy instanceof Pontan)
-            gui.drawElement(enemy.getPosition(), 'x', "#FF0000");
-        //TBC
+        if (enemy.getType() == Enemy.TYPE.BALLOOM) gui.drawElement(enemy.getPosition(), ':', "#FA732C");
+        else if (enemy.getType() == Enemy.TYPE.ONEAL) gui.drawElement(enemy.getPosition(), 'n', "#0000FF");
+        else if (enemy.getType() == Enemy.TYPE.DOLL) gui.drawElement(enemy.getPosition(), 'o', "#FF0000");
+        else if (enemy.getType() == Enemy.TYPE.MINVO) gui.drawElement(enemy.getPosition(), 'u', "#FA732C");
+        else if (enemy.getType() == Enemy.TYPE.KONDORIA) gui.drawElement(enemy.getPosition(), 't', "#0000FF");
+        else if (enemy.getType() == Enemy.TYPE.OVAPI) gui.drawElement(enemy.getPosition(), 'v', "#FF0000");
+        else if (enemy.getType() == Enemy.TYPE.PASS) gui.drawElement(enemy.getPosition(), 'w', "#FA732C");
+        else if (enemy.getType() == Enemy.TYPE.PONTAM) gui.drawElement(enemy.getPosition(), 'x', "#FF0000");
     }
 }
