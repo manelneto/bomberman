38115024ace3f2db6dd5575de4e4031b4e64196e
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-22 18:10:21 +0000

    ended bombermanViewer reviewing and refactoring: consistency

--- src/main/java/pt/up/fe/bomberman/viewer/game/BombermanViewer.java
+++ src/main/java/pt/up/fe/bomberman/viewer/game/BombermanViewer.java
@@ -1,13 +1,24 @@
 package pt.up.fe.bomberman.viewer.game;
 
 import pt.up.fe.bomberman.gui.GUI;
-import pt.up.fe.bomberman.model.Position;
 import pt.up.fe.bomberman.model.game.elements.Bomberman;
 
 public class BombermanViewer implements ElementViewer<Bomberman> {
     @Override
     public void draw(Bomberman bomberman, GUI gui) {
-        gui.drawBomberman(bomberman.getPosition(), bomberman.getDirection());
+        switch (bomberman.getDirection()) {
+            case 'U':
+                gui.drawElement(bomberman.getPosition(), 'd', "#64A4FF");
+                break;
+            case 'D':
+                gui.drawElement(bomberman.getPosition(), 'x', "#64A4FF");
+                break;
+            case 'L':
+                gui.drawElement(bomberman.getPosition(), 'z', "#64A4FF");
+                break;
+            case 'R':
+                gui.drawElement(bomberman.getPosition(), 'c', "#64A4FF");
+                break;
+        }
     }
-
 }
