887d20f5b5fe8664f3122973eed9b89be5adce89
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-23 16:54:02 +0000

    isBomb (T/F) tests for Arena

--- src/test/java/pt/up/fe/bomberman/model/game/arena/ArenaTest.java
+++ src/test/java/pt/up/fe/bomberman/model/game/arena/ArenaTest.java
@@ -2,10 +2,11 @@ package pt.up.fe.bomberman.model.game.arena;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pt.up.fe.bomberman.model.Position;
 import pt.up.fe.bomberman.model.game.elements.Bomb;
 import pt.up.fe.bomberman.model.game.elements.Flame;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArenaTest {
     Arena arena;
@@ -18,14 +19,25 @@ public class ArenaTest {
     @Test
     void addBomb() {
         arena.addBomb(new Bomb(1, 1, 0, 0));
-
         assertEquals(1, arena.getBombs().size());
     }
 
     @Test
     void addFlame() {
         arena.addFlame(new Flame(1, 1, 0, 'C'));
-
         assertEquals(1, arena.getFlames().size());
     }
+
+    @Test
+    void isBombTrue() {
+        arena.addBomb(new Bomb(1, 1, 0, 0));
+        assertTrue(arena.isBomb(new Position(1, 1)));
+    }
+
+    @Test
+    void isBombFalse() {
+        arena.addBomb(new Bomb(2, 2, 0, 0));
+        assertFalse(arena.isBomb(new Position(1, 1)));
+    }
+
 }
