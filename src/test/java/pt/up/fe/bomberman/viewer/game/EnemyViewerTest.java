9c87c367e5b037703e7f952296291c35f53ceb41
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-23 13:59:19 +0000

    separated tests for EnemyViewer

--- src/test/java/pt/up/fe/bomberman/viewer/game/EnemyViewerTest.java
+++ src/test/java/pt/up/fe/bomberman/viewer/game/EnemyViewerTest.java
@@ -5,44 +5,71 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import pt.up.fe.bomberman.gui.GUI;
 import pt.up.fe.bomberman.model.game.elements.Enemy;
-/*
-impublic class EnemyViewerTest {
+
+public class EnemyViewerTest {
     private Enemy enemy;
     private EnemyViewer viewer;
     private GUI gui;
 
     @BeforeEach
     void setUp() {
-        enemy = new Balloom(13, 17);
         viewer = new EnemyViewer();
         gui = Mockito.mock(GUI.class);
     }
 
     @Test
-    void drawEnemy() {
+    void drawElementBalloom() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.BALLOOM);
         viewer.draw(enemy, gui);
         Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), ':', "#FA732C");
-        enemy = new Oneal(13, 17);
+    }
+
+    @Test
+    void drawElementOneal() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.ONEAL);
         viewer.draw(enemy, gui);
         Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 'n', "#0000FF");
-        enemy = new Doll(13, 17);
+    }
+
+    @Test
+    void drawElementDoll() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.DOLL);
         viewer.draw(enemy, gui);
         Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 'o', "#FF0000");
-        enemy = new Kondria(13, 17);
+    }
+
+    @Test
+    void drawElementMinvo() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.MINVO);
         viewer.draw(enemy, gui);
-        Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 'u', "#0000FF");
-        enemy = new Minvo(13, 17);
+        Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 'u', "#FA732C");
+    }
+
+    @Test
+    void drawElementKondoria() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.KONDORIA);
         viewer.draw(enemy, gui);
-        Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 't', "#FA732C");
-        enemy = new Ovapi(13, 17);
+        Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 't', "#0000FF");
+    }
+
+    @Test
+    void drawElementOvapi() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.OVAPI);
         viewer.draw(enemy, gui);
         Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 'v', "#FF0000");
-        enemy = new Pass(13, 17);
+    }
+
+    @Test
+    void drawElementPass() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.PASS);
         viewer.draw(enemy, gui);
         Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 'w', "#FA732C");
-        enemy = new Pontan(13, 17);
+    }
+
+    @Test
+    void drawElementPontan() {
+        enemy = new Enemy(10, 10, Enemy.TYPE.PONTAM);
         viewer.draw(enemy, gui);
         Mockito.verify(gui, Mockito.times(1)).drawElement(enemy.getPosition(), 'x', "#FF0000");
     }
 }
-*/
